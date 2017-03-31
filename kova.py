b84340cbe76a9037c761ad8109b01fd7873c3b30
Junwon Park <junwonpk@stanford.edu>
2017-03-30 19:29:05 -0700

    story framework complete

--- kova.py
+++ kova.py
@@ -131,15 +131,14 @@ class Kova:
         else:
             user_data["username"] = username
             self.kovatype("Glad to meet you, " + username + "!")
-            #self.kovatype("I'd love to tell you my name too")
-            #self.kovatype("but..")
-            #self.kovatype("the truth is...")
-            #self.kovatype("I'm not sure what my name is...")
-            #self.kovatype("or where I'm from")
-            #self.kovatype("or where I am")
-            #self.kovatype("I'm just really scared and want to get out of here.")
+            self.kovatype("I'd love to tell you my name too")
+            self.kovatype("but..")
+            self.kovatype("the truth is...")
+            self.kovatype("I'm not sure what my name is...")
+            self.kovatype("or where I'm from..")
+            self.kovatype("or where I'm at..")
+            self.kovatype("I'm just really scared and want to get out of here.")
             self.next = 1
-        print 'done to this'
         return user_data
 
     def chapter3(self, input, user_data):
