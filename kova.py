6b11599092868ea9c237f7d3982990549af6d9cb
Junwon Park <junwonpk@stanford.edu>
2017-03-30 22:32:15 -0700

    story framework complete

--- kova.py
+++ kova.py
@@ -15,7 +15,7 @@ class Kova:
 
     def chat(self, input, user_id):
         #debugging
-        #self.redis.flushall()
+        self.redis.flushall()
         if input == 'redis flushall':
             self.redis.flushall()
         #debugging
