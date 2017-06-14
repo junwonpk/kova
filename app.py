217c93bfb9ca81458482f5e0c3e30196b0518cb7
Junwon Park <junwonpk@stanford.edu>
2017-06-14 16:52:33 -0700

    Auto Commit

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def webhook():
 
                 if messaging_event.get("message"):  # someone sent us a message
 
-                    time = messaging_event["message"]["timestamp"]
+                    time = messaging_event["timestamp"]
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     if "text" in messaging_event["message"].keys():
