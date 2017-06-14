211c90767287e301759b06f5f0ce2a12dd3813a9
Junwon Park <junwonpk@stanford.edu>
2017-06-14 15:07:19 -0700

    Auto Commit

--- kova.py
+++ kova.py
@@ -23,9 +23,9 @@ class Kova:
         for chapter in xrange(self.lastchapter + 1):
             self.chapters[chapter] = eval('self.chapter' + str(chapter))
         self.word2num = {"ten": 10, "eleven": 11, "twelve": 12, "thirteen": 13,\
-"fourteen":14, "fifteen":15, "sixteen":16, "seventeen":17, "eighteen":18 \
+"fourteen":14, "fifteen":15, "sixteen":16, "seventeen":17, "eighteen":18, \
 "nineteen":19, "twenty": 20, "thirty": 30, "fourty": 40, "fifty": 50, "sixty": 60,
-"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7\
+"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7,\
 "eight": 8, "nine": 9}
 
     def chat(self, input, user_id):
