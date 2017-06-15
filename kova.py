4944e1afb0f6929a39b8a7efea6571873e3c7d34
Junwon Park <junwonpk@stanford.edu>
2017-06-14 19:40:49 -0700

    Auto Commit

--- kova.py
+++ kova.py
@@ -27,7 +27,7 @@ class Kova:
 "fourteen":14, "fifteen":15, "sixteen":16, "seventeen":17, "eighteen":18, \
 "nineteen":19, "twenty": 20, "thirty": 30, "fourty": 40, "fifty": 50, "sixty": 60,
 "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7,\
-"eight": 8, "nine": 9}
+"eight": 8, "nine": 9, "thousand": 1000}
 
     def chat(self, input, user_id, time):
         self.preprocess(input, user_id)
